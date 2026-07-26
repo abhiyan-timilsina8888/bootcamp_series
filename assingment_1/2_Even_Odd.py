e7c5a9d82b31e04fafa2f8f6a46776c11452958e
abhiyan-timilsina8888 <abhiyantimilsina96@gmail.com>
2026-07-26 15:59:37 +0545

    testing git update

--- assingment_1/2_Even_Odd.py
+++ assingment_1/2_Even_Odd.py
@@ -1,7 +1,2 @@
 n= int(input("Enter the number\n"))
-if n==0:
-    print("Zero")
-elif n % 2==0:
-    print(f"{n} is a even number")
-else:
-    print(f'{n} is a odd number')
+print("even" if n%2==0 else "odd" )
